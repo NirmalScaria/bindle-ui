03c41bbf096937661ba99c72da7f096f1d081842
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-08-03 21:36:50 +0530

    [FEATURE] Fetch component remotely

--- scripts/add.mjs
+++ scripts/add.mjs
@@ -2,33 +2,39 @@
 
 import { Command } from "commander";
 import { getConfig } from "./utils/getConfig.mjs";
-import { buttonComponent } from "./components/button.mjs";
 import fs from "fs/promises";
 import path from "path";
+import { fetchComponent } from "./utils/fetchComponent.mjs";
+import chalk from 'chalk';
 
 export const add = new Command("add")
     .description("Add a new component")
     .argument("<component>", "Name of the component to be added.")
-    .action((component) => {
-        addAction(component);
+    .action((componentId) => {
+        addAction(componentId);
     })
 
-async function addAction(component) {
-    console.log(`Adding component ${component}`)
+async function addAction(componentId) {
+    console.log(`Adding component ${componentId}`)
+    const component = await fetchComponent(componentId, true)
     const config = await getConfig();
-    const targetDirectory = path.join(process.cwd(), config.componentDirectory, buttonComponent.location);
+    if(component === undefined) {
+        console.log(chalk.red("Component not found! Make sure the name is correct."));
+        return;
+    }
+    const targetDirectory = path.join(process.cwd(), config.componentDirectory, component.location);
     try {
         await fs.access(targetDirectory);
     } catch (error) {
         await fs.mkdir(targetDirectory, { recursive: true });
     }
-    const targetFile = path.join(targetDirectory, `${buttonComponent.name}.tsx`)
+    const targetFile = path.join(targetDirectory, `${component.name}.tsx`)
     await fs
-        .writeFile(targetFile, buttonComponent.content)
+        .writeFile(targetFile, component.content)
         .then(() => {
-            console.log("Component added successfully!");
+            console.log(chalk.green("✔ Component added successfully!"));
         })
         .catch((error) => {
-            console.error("Error adding component: ", error);
+            console.error(chalk.red("Error adding component: ", error));
         });
 }
---
+++ scripts/utils/fetchComponent.mjs
@@ -0,0 +1,18 @@
+import chalk from "chalk";
+
+export async function fetchComponent(componentId, useTypescript) {
+    const url = `https://bindle-ui-dash.vercel.app/api/getComponent?componentId=${componentId}&useTypescript=${useTypescript}`;
+    try {
+        var response = await fetch(url);
+        response = await response.json();
+        if(!response.success) {
+            console.error(chalk.red(response.message));
+            return;
+        }
+    const content = JSON.parse(response.component);
+    return content
+    }
+    catch (error) {
+        console.error(chalk.red("Error fetching component: ", error));
+    }
+}
