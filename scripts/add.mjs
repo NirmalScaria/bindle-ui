603c47b771430d80dc858c7d6107cfbed31803cc
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-08-06 17:37:07 +0530

    [FEATURE] Install dependancies

--- scripts/add.mjs
+++ scripts/add.mjs
@@ -6,6 +6,7 @@ import fs from "fs/promises";
 import path from "path";
 import { fetchComponent } from "./utils/fetchComponent.mjs";
 import chalk from 'chalk';
+import { execSync } from "child_process";
 
 export const add = new Command("add")
     .description("Add a new component")
@@ -16,9 +17,23 @@ export const add = new Command("add")
 
 async function addAction(componentId) {
     console.log(`Adding component ${componentId}`)
+    var relativeImports = [];
+    var remoteDependencies = [];
+    var relativeImportLocations = {};
+    var filesToAdd = {}
+    var tailwindAdditionalConfig = {};
     const component = await fetchComponent(componentId, true)
-    if(component === undefined) {
-        console.log(chalk.red("Component not found! Make sure the name is correct."));
+    await parseTree({ currentComponent: component });
+    var remoteInstallCommand = "npm install ";
+    for (const remoteDependancy of remoteDependencies) {
+        remoteInstallCommand += `${remoteDependancy.name}@${remoteDependancy.version} `;
+    }
+    if (remoteDependencies.length > 0) {
+        console.log(chalk.blue("Installing remote dependancies..."));
+        execSync(remoteInstallCommand, { stdio: 'inherit' });
+    }
+    if (component === undefined) {
+        console.log(chalk.red("Failed to install the component."));
         return;
     }
     var targetFile = path.join(process.cwd(), component.location);
@@ -36,4 +51,30 @@ async function addAction(componentId) {
         .catch((error) => {
             console.error(chalk.red("Error adding component: ", error));
         });
+
+    async function parseTree({ currentComponent }) {
+        filesToAdd[currentComponent.location] = currentComponent.content;
+        relativeImportLocations[currentComponent.id] = currentComponent.location;
+        if (currentComponent.tailwindConfig != "") {
+            const additionalConfig = JSON.parse(currentComponent.tailwindConfig);
+            if (additionalConfig) {
+                tailwindAdditionalConfig = { ...tailwindAdditionalConfig, ...additionalConfig };
+            }
+        }
+        if (currentComponent.relativeImports.length > 0) {
+            for (const relativeImport of currentComponent.relativeImports) {
+                if (relativeImports.includes(relativeImport)) return;
+                relativeImports.push(relativeImport);
+                const nextComponent = await fetchComponent(relativeImport)
+                await parseTree({ currentComponent: nextComponent });
+            }
+        }
+        if (currentComponent.remoteDependancies.length > 0) {
+            for (const remoteDependancy of currentComponent.remoteDependancies) {
+                if (remoteDependencies.includes(remoteDependancy)) return;
+                remoteDependencies.push(remoteDependancy);
+            };
+        }
+    }
 }
+
